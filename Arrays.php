c9f62a0edbd88101bf5fbe2ad729fb413fe4c31d
sRevko <stas.revko@gmail.com>
2017-04-16 00:02:42 +0300

    init

--- Arrays.php
+++ Arrays.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Stas
+ * User: successus
+ * Author: Stanislav Revko
  * Date: 15.04.2017
  * Time: 23:51
  */
@@ -11,5 +11,19 @@ namespace successus\helpers;
 
 class Arrays
 {
+    /**
+     * Creates an array by using one array for keys and another for its values
+     * Example: print_r(array_combine(Array('a','a','b'), Array(1,2,3)));  Result: Array([a] => 2, [b] => 3)
+     * @param $keys
+     * @param $values
+     * @return array
+     */
+    public static function combine(array $keys, array $values ){
+        $res = [];
+        foreach($keys as $i => $key){
+            $res[$key] = isset($values[$i])? $values[$i] : null;
+        }
+        return $res;
+    }
 
 }
--- Csv.php
+++ Csv.php
@@ -11,5 +11,27 @@ namespace successus\helpers;
 
 class Csv
 {
+    /**
+     * Парсить csv файл в список объектов или массивов данных
+     * @param string $file -  content of csv file
+     * @param string $delimiter
+     * @return array
+     */
+    static function parse($file='', $delimiter=',')
+    {
+        $result = [];
+        $rows = explode("\n", $file);
+
+        $header = explode($delimiter,array_shift($rows));
+        $countOfColums = count($header);
+        foreach ($rows as $k=>$row){
+            $values = explode($delimiter, $row);
+            if($values){
+                $result[] = Arrays::combine($header, $values);
+            }
+        }
+
+        return $result;
+    }
 
 }
